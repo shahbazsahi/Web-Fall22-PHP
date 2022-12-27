d191180fa19117e7dbdfd540527169408613f0e4
shahbazsahi@ntu.edu.pk <shahbazsahi@ntu.edu.pk>
2022-12-27 09:40:57 +0500

    PHP till messages

---
+++ userhome.php
@@ -0,0 +1,38 @@
+<?php
+/*
+  if(!isset($_COOKIE['loggedIn'])){
+    header('location:signin.php?rid=5');
+  }
+  if(isset($_GET['rid'])){
+      $id = $_GET['rid'];
+  }*/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Home</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+<div class="container mt-3">
+  <div class="alert alert-success">
+    <strong>Welcome!</strong> 
+    <?php
+      if($id == 1){
+        print('Signin successfull');
+      }
+
+      elseif($id == 3){
+        print('Signup successfull');
+      }
+    ?>
+  </div>
+</div>
+
+</body>
+</html>
